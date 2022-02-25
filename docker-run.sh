e87a319b58a331202f7107fc98375ac65a24d8ba
SeongIkKim <kpic1638@gmail.com>
2022-02-26 01:51:25 +0900

    chore: container name

--- docker-run.sh
+++ docker-run.sh
@@ -8,7 +8,7 @@ ENV_STATE="dev"
 set -e # stop if error occurs
 set -x # execute after print command
 
-docker run -d --name=fastapi-container \
+docker run -d --name=inference-server \
   -e ENV_STATE=${ENV_STATE} \
   -p 80:80 \
   ${IMAGE_NAME}:${TAG_NAME}
